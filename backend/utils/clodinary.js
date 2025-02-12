9bfe2f84ebc6f9efe40d743ccb43d35a871e5f06
Fenil Patel <your-email@example.com>
2025-02-13 04:51:20 +0530

    hfghgf

--- backend/utils/clodinary.js
+++ backend/utils/clodinary.js
@@ -1,11 +1,11 @@
 import { v2 as cloudinary } from "cloudinary";
-import logger from "./logger";
+import logger from "./logger.js";
 import dotenv from "dotenv";
 import fs from "fs";
 
 dotenv.config();
 
-Configuration;
+//Configuration;
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
